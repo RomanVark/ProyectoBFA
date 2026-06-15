ae3f1d5995b21662501ddd8861584e9a2accde8d
CatilloZzz <jireyes@uamv.edu.ni>
2026-06-15 16:14:18 -0600

    Feat: se agregaron los atributos a la clase usuario

--- BFA/src/main/java/ni/edu/uam/BFA/modelo/Usuario.java
+++ BFA/src/main/java/ni/edu/uam/BFA/modelo/Usuario.java
@@ -1,5 +1,35 @@
 package ni.edu.uam.BFA.modelo;
 
-public class Usuario {
+import lombok.*;
+import javax.persistence.*;
+import org.openxava.annotations.*;
 
-}
+
+    @MappedSuperclass
+    public abstract class Usuario {
+
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        @Hidden
+        private Integer idUsuario;
+
+        @Column(nullable = false, length = 100)
+        @Required
+        private String nombre;
+
+        @Column(nullable = false, unique = true, length = 150)
+        @Required
+        private String correo;
+
+
+
+        public Integer getIdUsuario() { return idUsuario; }
+        public void setIdUsuario(Integer idUsuario) { this.idUsuario = idUsuario; }
+
+        public String getNombre() { return nombre; }
+        public void setNombre(String nombre) { this.nombre = nombre; }
+
+        public String getCorreo() { return correo; }
+        public void setCorreo(String correo) { this.correo = correo; }
+    }
+
